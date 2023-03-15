ec12a6fe865d52afd8649af22b5a6c9f3777f797
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-15 21:11:00 +0300

    feat: header / contacts (desktop)

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 
 import React from 'react';
+import { telegramLogo, viberLogo, whatsAppLogo } from '../images/svg';
 
 function Contacts({ place }) {
   return (
@@ -9,13 +10,13 @@ function Contacts({ place }) {
       <a className="contacts__phone" href="tel:+75555555555">+7 555 555-55-55</a>
       <ul className="contacts__social">
         <li className="contacts__social-item">
-          <a className="contacts__social-link" href="#" target="_blank"></a>
+          <a className="contacts__social-link" href="#" target="_blank">{telegramLogo}</a>
         </li>
         <li className="contacts__social-item">
-          <a className="contacts__social-link" href="#" target="_blank"></a>
+          <a className="contacts__social-link" href="#" target="_blank">{viberLogo}</a>
         </li>
         <li className="contacts__social-item">
-          <a className="contacts__social-link" href="#" target="_blank"></a>
+          <a className="contacts__social-link" href="#" target="_blank">{whatsAppLogo}</a>
         </li>
       </ul>
       { place === "footer" && <p className="contacts__address">Москва, Путевой проезд 3с1, к 902</p> }
