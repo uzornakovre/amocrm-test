da70462b6a68b8e5714c0d457ef7df7492c251f1
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-16 01:18:56 +0300

    feat: links list

---
+++ src/utils/links.js
@@ -0,0 +1,53 @@
+export const companyLinks = [
+  {
+    title: 'Партнерская программа',
+    link: '#'
+  },
+  {
+    title: 'Вакансии',
+    link: '#'
+  },
+];
+
+export const menuLinks = [
+  {
+    title: 'Расчет стоимости',
+    link: '#'
+  },
+  {
+    title: 'Услуги',
+    link: '#'
+  },
+  {
+    title: 'Виджеты',
+    link: '#'
+  },
+  {
+    title: 'Интеграции',
+    link: '#'
+  },
+  {
+    title: 'Наши клиенты',
+    link: '#'
+  },
+  {
+    title: 'Кейсы',
+    link: '#'
+  },
+  {
+    title: 'Благодарственные письма',
+    link: '#'
+  },
+  {
+    title: 'Сертификаты',
+    link: '#'
+  },
+  {
+    title: 'Блог на YouTube',
+    link: '#'
+  },
+  {
+    title: 'Вопрос-ответ',
+    link: '#'
+  },
+]
