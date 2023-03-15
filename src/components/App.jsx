2e9943891b68ca8de38c4d91da0a094a4db27c1c
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-15 23:39:27 +0300

    feat: main (desktop)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Main from './Main'
 
 function App() {
-
   return (
     <div className="page__content">
       <Header />
+      <Main />
     </div>
   );
 }
