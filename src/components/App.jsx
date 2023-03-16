023bc920f076ca6e64aaca42bac07f5e0eae2dab
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-16 03:21:10 +0300

    feat: footer (desktop)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
-import Main from './Main'
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="page__content">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
